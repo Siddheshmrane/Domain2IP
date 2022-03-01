c6da134df2738055b5d8b4ab4491beb6e593d62d
Siddheshmrane <93186500+Siddheshmrane@users.noreply.github.com>
2022-02-28 19:18:45 -0800

    Add files via upload

---
+++ domainToIp.py
@@ -0,0 +1,23 @@
+import socket
+import xlwt 
+from xlwt import Workbook
+
+file = open('/home/siddhesh/Documents/SOC Tools/D2IP/D2IP.txt','r')
+
+domain_list = []
+wb = Workbook()
+
+for x in file.readlines():
+    domain_list.append(x.rstrip())
+
+sheet1 = wb.add_sheet('Sheet 1')
+#sheet1.write(1,0,'abc')
+
+for i in range(len(domain_list)):
+    sheet1.write(i,0,domain_list[i])
+    sheet1.write(i,1,socket.gethostbyname(domain_list[i]))
+
+#for y in domain_list:
+    #print(y + ' -> '+ socket.gethostbyname(y))
+
+wb.save('D2IP_output.xls')
